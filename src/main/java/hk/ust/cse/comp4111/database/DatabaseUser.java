ba058d01a3786a4929c432f82f24ff3f85302b55
STommydx <ttoo74@gmail.com>
2020-03-08 21:45:34 +0800

    Clean up code using try with resources

--- src/main/java/hk/ust/cse/comp4111/database/DatabaseUser.java
+++ src/main/java/hk/ust/cse/comp4111/database/DatabaseUser.java
@@ -7,18 +7,13 @@ import java.sql.SQLException;
 
 public class DatabaseUser {
     public static boolean authenticate(Connection connection, String username, String password) throws SQLException {
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
         boolean result = false;
-        try {
-            statement = connection.prepareStatement("SELECT username FROM users WHERE username = ? AND password = ?");
+        try (PreparedStatement statement = connection.prepareStatement("SELECT username FROM users WHERE username = ? AND password = ?")) {
             statement.setString(1, username);
             statement.setString(2, password);
-            resultSet = statement.executeQuery();
-            if (resultSet.next()) result = true;
-        } finally {
-            if (resultSet != null) resultSet.close();
-            if (statement != null) statement.close();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) result = true;
+            }
         }
         return result;
     }
