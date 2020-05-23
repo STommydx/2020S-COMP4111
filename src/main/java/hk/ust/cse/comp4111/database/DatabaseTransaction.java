992caf51f625836f14db25d0f57419e3d5b2b513
STommydx <ttoo74@gmail.com>
2020-05-23 23:29:52 +0800

    remove unused import

--- src/main/java/hk/ust/cse/comp4111/database/DatabaseTransaction.java
+++ src/main/java/hk/ust/cse/comp4111/database/DatabaseTransaction.java
@@ -5,9 +5,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 public class DatabaseTransaction {
     public static boolean commit(@NotNull Transaction transaction) throws SQLException {
